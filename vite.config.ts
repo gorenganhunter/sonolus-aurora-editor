347d838778b9b440f0ba53d02733edd76f5c22a0
Gorengan Hunter <gorengandev@gmail.com>
2026-02-09 03:32:28 +0000

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import vue from '@vitejs/plugin-vue'
 import autoprefixer from 'autoprefixer'
 import tailwind from 'tailwindcss'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,43 @@ export default defineConfig({
             plugins: [tailwind(), autoprefixer()],
         },
     },
-    plugins: [vue()],
+    plugins: [vue(), VitePWA({
+        manifest: {
+            "name": "Sonolus Aurora Editor",
+            "short_name": "Ædit",
+            "icons": [
+                {
+                    "src": "/pwa-192x192.png",
+                    "sizes": "192x192",
+                    "type": "image/png",
+                    "purpose": "any"
+                },
+                {
+                    "src": "/pwa-512x512.png",
+                    "sizes": "512x512",
+                    "type": "image/png",
+                    "purpose": "any"
+                },
+                {
+                    "src": "/pwa-maskable-192x192.png",
+                    "sizes": "192x192",
+                    "type": "image/png",
+                    "purpose": "maskable"
+                },
+                {
+                    "src": "/pwa-maskable-512x512.png",
+                    "sizes": "512x512",
+                    "type": "image/png",
+                    "purpose": "maskable"
+                }
+            ],
+            "start_url": "/",
+            "display": "standalone",
+            "background_color": "#6666ff",
+            "theme_color": "#77EFDC",
+            "description": "Editor for Sonolus Aurora"
+        },
+        registerType: "autoUpdate",
+        workbox: { globPatterns: ["**/*"] }
+    })]
 })
