1e5e6d731d1462c7a4e5a3490e1530e847e75078
Burrito <burritoyangster@gmail.com>
2026-07-10 08:51:39 +0800

    Fix field styling with long label (#177)

--- src/modals/form/BaseField.vue
+++ src/modals/form/BaseField.vue
@@ -5,8 +5,8 @@ defineProps<{
 </script>
 
 <template>
-    <label class="flex flex-col justify-between gap-1 sm:flex-row">
-        <div class="sm:flex sm:h-8 sm:w-[48rem] sm:items-center">
+    <label class="flex flex-col justify-between gap-1 sm:flex-row sm:items-center">
+        <div class="sm:flex sm:min-h-8 sm:w-[48rem] sm:items-center">
             {{ label }}
         </div>
         <slot />
