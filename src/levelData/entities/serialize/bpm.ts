6e0402e38261e2f41a0c227363c5906906904b32
Burrito <burritoyangster@gmail.com>
2026-07-10 08:45:39 +0800

    Renovate (#174)

--- src/levelData/entities/serialize/bpm.ts
+++ src/levelData/entities/serialize/bpm.ts
@@ -3,18 +3,16 @@ import { getStoreEntities } from '.'
 import type { Store } from '../../../state/store'
 
 export const serializeBpmsToLevelDataEntities = (store: Store) =>
-    [...getStoreEntities(store.grid.bpm)].map(
-        (bpm): LevelDataEntity => ({
-            archetype: EngineArchetypeName.BpmChange,
-            data: [
-                {
-                    name: EngineArchetypeDataName.Beat,
-                    value: bpm.beat,
-                },
-                {
-                    name: EngineArchetypeDataName.Bpm,
-                    value: bpm.bpm,
-                },
-            ],
-        }),
-    )
+    [...getStoreEntities(store.grid.bpm)].map((bpm): LevelDataEntity => ({
+        archetype: EngineArchetypeName.BpmChange,
+        data: [
+            {
+                name: EngineArchetypeDataName.Beat,
+                value: bpm.beat,
+            },
+            {
+                name: EngineArchetypeDataName.Bpm,
+                value: bpm.bpm,
+            },
+        ],
+    }))
